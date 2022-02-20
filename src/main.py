0e6495ac469084222c36ab02000e801484ed09d1
RodrigoNavarroNogueira <rodrigo.navarro36@hotmail.com>
2022-02-20 19:11:10 -0300

    Adicionando o menu para escolhas entre dolar e euro

--- src/main.py
+++ src/main.py
@@ -7,20 +7,27 @@ def mensagem_de_abertura():
 	print('*-' * 30)
 
 
+def escolha_a_opcao():
+    option = int(input("""
+Selecione a opção que você deseja:
+[ 1 ] - Visualizar Dolar
+[ 2 ] - Visualizar Euro
+    """))
+    if option == 1:
+        request = requests.get(f'http://economia.awesomeapi.com.br/json/last/USD-BRL')
+    elif option == 2:
+        request = requests.get(f'https://economia.awesomeapi.com.br/last/EUR-BRL')
+
+
+    coins = request.json()
+
+    if 'erro' not in coins:
+	    print(request.json())
+
+
+
+
 mensagem_de_abertura()
-
-request = requests.get(f'https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL')
-
-coins = request.json()
-
-if 'erro' not in coins:
-	print(request.json())
-
-else:
-	print('Moeda inválida')
-	while True:
-		new_try = int(input('Você deseja realizar uma nova consulta? (1) Para SIM e (2) Para NÃO\n'))
-
-
+escolha_a_opcao()
 
 
