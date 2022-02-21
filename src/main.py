2fc22ef7b97937d317369d881b7f224bbf4e5667
RodrigoNavarroNogueira <rodrigo.navarro36@hotmail.com>
2022-02-21 11:37:16 -0300

    Formatando a resposta da requisição para o dolar e o euro

--- src/main.py
+++ src/main.py
@@ -13,17 +13,18 @@ Selecione a opção que você deseja:
 [ 1 ] - Visualizar Dolar
 [ 2 ] - Visualizar Euro
     """))
+    
     if option == 1:
         request = requests.get(f'http://economia.awesomeapi.com.br/json/last/USD-BRL')
+        
     elif option == 2:
         request = requests.get(f'https://economia.awesomeapi.com.br/last/EUR-BRL')
 
 
     coins = request.json()
-
-    if 'erro' not in coins:
-	    print(request.json())
-
+    bid = coins["USDBRL"]
+    print(f'Valor do dólar atualizado: R${bid["bid"][:4]}')
+    print(f'Dia e hora da consulta: {bid["create_date"]}')
 
 
 
