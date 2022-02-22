ccadcace09c8c73fa91c77e5787cbd352d93820a
RodrigoNavarroNogueira <rodrigo.navarro36@hotmail.com>
2022-02-22 11:19:32 -0300

    Opção de visualizar dolar e euro funcionando corretamente

--- src/main.py
+++ src/main.py
@@ -7,6 +7,13 @@ def mensagem_de_abertura():
 	print('*-' * 30)
 
 
+def exibir_consulta(request, coin, moeda):
+    coins = request.json()
+    bid = coins[coin]
+    print(f'Valor do {moeda} atualizado: R${bid["bid"][:4]}')
+    print(f'Dia e hora da consulta: {bid["create_date"]}')
+
+
 def escolha_a_opcao():
     option = int(input("""
 Selecione a opção que você deseja:
@@ -16,19 +23,18 @@ Selecione a opção que você deseja:
     
     if option == 1:
         request = requests.get(f'http://economia.awesomeapi.com.br/json/last/USD-BRL')
+        coin = "USDBRL"
+        moeda = "dólar"
         
     elif option == 2:
         request = requests.get(f'https://economia.awesomeapi.com.br/last/EUR-BRL')
+        coin = "EURBRL"
+        moeda = "euro"
 
-
-    coins = request.json()
-    bid = coins["USDBRL"]
-    print(f'Valor do dólar atualizado: R${bid["bid"][:4]}')
-    print(f'Dia e hora da consulta: {bid["create_date"]}')
+    exibir_consulta(request, coin, moeda)
 
 
 
 mensagem_de_abertura()
 escolha_a_opcao()
-
 
