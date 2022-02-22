a3175df9e980c64a09d0f93dced8c3a91aa71c5c
RodrigoNavarroNogueira <rodrigo.navarro36@hotmail.com>
2022-02-22 11:54:53 -0300

    Adicionando a opção para repetir o processo sem reiniciar o programa

--- src/main.py
+++ src/main.py
@@ -12,7 +12,8 @@ def exibir_consulta(request, coin, moeda):
     bid = coins[coin]
     print(f'Valor do {moeda} atualizado: R${bid["bid"][:4]}')
     print(f'Dia e hora da consulta: {bid["create_date"]}')
-
+    print(f'Dia da consulta: {bid["create_date"][:10]}')
+    
 
 def escolha_a_opcao():
     option = int(input("""
@@ -25,7 +26,7 @@ Selecione a opção que você deseja:
         request = requests.get(f'http://economia.awesomeapi.com.br/json/last/USD-BRL')
         coin = "USDBRL"
         moeda = "dólar"
-        
+
     elif option == 2:
         request = requests.get(f'https://economia.awesomeapi.com.br/last/EUR-BRL')
         coin = "EURBRL"
@@ -33,6 +34,16 @@ Selecione a opção que você deseja:
 
     exibir_consulta(request, coin, moeda)
 
+    nova_consulta = str()
+
+    while nova_consulta == '' or nova_consulta not in 'SN':
+        nova_consulta = input('Deseja realizar uma nova consulta? (S) para SIM e (N) para NÃO\n').upper()
+
+    if nova_consulta == "S":
+        escolha_a_opcao()
+
+    elif nova_consulta == "N":
+        print('Encerrando programa, até logo!')
 
 
 mensagem_de_abertura()
